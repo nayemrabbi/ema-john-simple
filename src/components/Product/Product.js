c6a1e115316a6ffc277aac4929c16d30bbba2c69
nayemrabbi <nrabbi121@gmail.com>
2020-03-23 21:58:49 +0600

    Project Done

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,33 +2,37 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 import "./Product.css";
+import { Link } from "react-router-dom";
 
 const Product = props => {
-  const { img, name, seller, price, stock } = props.product;
+  console.log(props);
+  const { img, name, seller, price, stock, key } = props.product;
   return (
     <div className="product">
       <div>
         <img src={img} alt="" />
       </div>
       <div>
-        <h4 className="product-name">{name}</h4>
+        <h4 className="product-name">
+          <Link to={"/product/" + key}>{name}</Link>
+        </h4>
         <p>
-          {" "}
-          <small>By: {seller} </small>{" "}
+          <small>By: {seller} </small>
         </p>
         <p>
-          {" "}
           <small>${price} </small>
         </p>
         <p>
           <small>Only {stock} left in stock- Order Soon</small>
         </p>
-        <button
-          className="main-button"
-          onClick={() => props.handleAddProduct(props.product)}
-        >
-          <FontAwesomeIcon icon={faShoppingCart} /> Add to Cart
-        </button>
+        {props.showAddToCart === true && (
+          <button
+            className="main-button"
+            onClick={() => props.handleAddProduct(props.product)}
+          >
+            <FontAwesomeIcon icon={faShoppingCart} /> Add to Cart
+          </button>
+        )}
       </div>
     </div>
   );
---
+++ src/components/ReviewItem/ReviewItem.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./ReviewItem.css";
+
+const ReviewItem = props => {
+  const { name, quantity, key, price } = props.product;
+
+  return (
+    <div className="review-item">
+      <h4 className="product-name">{name}</h4>
+      <p>{quantity}</p>
+      <p>
+        <small>${price}</small>
+      </p>
+      <button onClick={() => props.removeProduct(key)} className="main-button">
+        Remove
+      </button>
+    </div>
+  );
+};
+
+export default ReviewItem;
